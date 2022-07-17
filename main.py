29bc025e0631074adf60f6122746c4b3576ab39d
xkuubix <buler.jakub@gmail.com>
2022-07-18 01:34:15 +0200

    added LRfinding

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # %%
 import torch
-import os
 from torchvision import transforms
 from my_drawings import my_show_image, my_show_training_results
 from my_stats import make_df, show_stats
-from BreastCancerDataset import BreastCancerDataset
 from torchvision import models
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -12,8 +10,10 @@ from torch import nn
 from net_utils import train_net, test_net
 from gen_data_loader import gen_data_loader
 from random_split_df import random_split_df
+from LRFinder import LRFinder, plot_lr_finder
 
 # ------------
+# import os
 # from pydicom import dcmread
 # os.chdir(os.path.join(root, 'Malignant'))
 # ds = dcmread(os.listdir(os.getcwd())[1])
@@ -31,30 +31,26 @@ file_dir = '/media/dysk/student2/mammografia/Zapisy/stats_pickle'
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
-transform = None  # normalize
+transform = transforms.Compose([normalize, transforms.Resize((224, 224))])
 
+transform = None
 df = make_df(root, from_file=True, save_to_file=False, file_dir=file_dir)
 # data = df.to_dict('records')  # list of patients as dicts
 # %%
 train_set, val_set, test_set = random_split_df(df=df, seed=50)
-data_loaders, data_loaders_sizes = gen_data_loader(root,
-                                                   train_set,
-                                                   val_set,
-                                                   test_set,
-                                                   'CC',
-                                                   transforms=None)
+data_loaders, data_loaders_sizes, ds = gen_data_loader(root,
+                                                       train_set,
+                                                       val_set,
+                                                       test_set,
+                                                       'CC',
+                                                       transforms=transform,
+                                                       batch_size=16)
 
 # %%
-# make_df(cc_list)
-# if 0:
-#     for i in range(10):
-#         my_show_image(normal_cc[i], with_marks=True)
-#     for i in range(10):
-#         my_show_image(benign_cc[i], with_marks=True)
-#     for i in range(10):
-#         my_show_image(malignant_cc[i], with_marks=True)
-#     for i in range(10):
-#         my_show_image(lymph_nodes_cc[i], with_marks=True)
+if 1:
+    for i in range(1):
+        my_show_image(ds['train'][i], with_marks=True)
+
 # if 0:
 #     show_stats(cc_list)
 
@@ -62,26 +58,60 @@ data_loaders, data_loaders_sizes = gen_data_loader(root,
 
 # %%
 net = models.resnet18(pretrained=True)
+
 num_features = net.fc.in_features
 net.fc = nn.Linear(num_features, 1)  # dla BCE 1, dla CCE len(class_names)
 net = net.to(device)
-# %%
-# criterion = nn.CrossEntropyLoss()
-criterion = nn.BCELoss()
-optimizer = optim.SGD(net.parameters(), lr=0.005, momentum=0.9,
-                      weight_decay=0.001)
-# Decay LR by a factor of 0.1 every x epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
+print(sum(p.numel() for p in net.parameters() if p.requires_grad))
 
+# print(f'The model has {count_parameters(net):,} trainable parameters')
+
+# %%
+
+START_LR = 1e-7
+optimizer = optim.Adam(net.parameters(), lr=START_LR)
+criterion = nn.BCELoss()
+criterion = criterion.to(device)
+END_LR = 1
+NUM_ITER = 300
+
+if 1:
+    lr_finder = LRFinder(net, optimizer, criterion, device)
+    lrs, losses = lr_finder.range_test(data_loaders['train'], END_LR, NUM_ITER)
+    plot_lr_finder(lrs, losses, skip_start=0, skip_end=0)
+
+FOUND_LR = 0  # 2e-4 for adamoptim resnet50 bs32 resize224
+
+params = [
+          {'params': net.conv1.parameters(), 'lr': FOUND_LR / 10},
+          {'params': net.bn1.parameters(), 'lr': FOUND_LR / 10},
+          {'params': net.layer1.parameters(), 'lr': FOUND_LR / 8},
+          {'params': net.layer2.parameters(), 'lr': FOUND_LR / 6},
+          {'params': net.layer3.parameters(), 'lr': FOUND_LR / 4},
+          {'params': net.layer4.parameters(), 'lr': FOUND_LR / 2},
+          {'params': net.fc.parameters()}
+         ]
+
+optimizer = optimizer = optim.SGD(params, lr=FOUND_LR,
+                                  momentum=0.9, weight_decay=0.001)
+
+EPOCHS = 10
+STEPS_PER_EPOCH = data_loaders_sizes['train']
+TOTAL_STEPS = EPOCHS * STEPS_PER_EPOCH
+
+MAX_LRS = [p['lr'] for p in optimizer.param_groups]
+
+scheduler = lr_scheduler.OneCycleLR(optimizer,
+                                    max_lr=MAX_LRS,
+                                    total_steps=TOTAL_STEPS)
 # %% ---------------------------------------------------------
 net, loss_stats, accuracy_stats = train_net(net, data_loaders,
                                             data_loaders_sizes,
                                             device, criterion,
-                                            optimizer, exp_lr_scheduler,
-                                            num_epochs=200)
-# %% tymczasowe wykresy jak sie policzy
+                                            optimizer, scheduler,
+                                            num_epochs=EPOCHS)
+# %%
 my_show_training_results(accuracy_stats, loss_stats)
-
 # %% lr finder, learner fastai, metrics sckitlearn
 test_net(net, data_loaders, class_names, device)
 # %%
