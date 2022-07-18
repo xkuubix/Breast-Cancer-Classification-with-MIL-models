0d8d35d9c44e3c2dd49a11e942be501d36dadd67
xkuubix <buler.jakub@gmail.com>
2022-07-18 14:47:26 +0200

    v2

--- get_points_to_crop.py
+++ get_points_to_crop.py
@@ -9,26 +9,27 @@ def get_points(ds, show_images_count: int):
     i = 0
     for d_s in ['train', 'val', 'test']:
         for image, _ in ds[d_s]:
-            # my_mask = image[0].detach()
-            # my_mask[my_mask < 1e-18] = 0
-            # my_mask[my_mask > 1e-18] = 1
-            # np_im = my_mask.numpy()
-            # rows = np.argwhere(np.sum(np_im, axis=1) > 10)
-            # cols = np.argwhere(np.sum(np_im, axis=0) > 10)
-            # col1 = np.argmax(cols)
-            # row1 = rows[0]
-            # row2 = rows[-1]
-            # r_up = np.append(r_up, row1)
-            # r_down = np.append(r_down, row2)
-            # c = np.append(c, col1)
-            # if i < show_images_count or col1 > 2700:
-            if i == 829:
+            my_mask = image[0].detach()
+            my_mask[my_mask < 1e-18] = 0
+            my_mask[my_mask > 1e-18] = 1
+            np_im = my_mask.numpy()
+            rows = np.argwhere(np.sum(np_im, axis=1) > 10)
+            cols = np.argwhere(np.sum(np_im, axis=0) > 10)
+            col1 = np.argmax(cols)
+            row1 = rows[0]
+            row2 = rows[-1]
+            if row1 > 1 and row2 < 3500 and col1 < 2800:
+                r_up = np.append(r_up, row1)
+                r_down = np.append(r_down, row2)
+                c = np.append(c, col1)
+            if i < show_images_count:
                 # new_image = np_im[row1[0]-50:row2[0]+50, 0:col1+50]
-                # plt.imshow(new_image)
-                # plt.show()
-                plt.imshow(image[0])
-                plt.title(str(i))
+                new_image = np_im[:, 0:2400]
+                plt.imshow(new_image)
                 plt.show()
+                # plt.imshow(image[0])
+                # plt.title(str(i))
+                # plt.show()
             i += 1
     print(np.min(r_up), np.max(r_down), np.max(c))
     return np.min(r_up), np.max(r_down), np.max(c)
