713207b057e11fea50d470de201b78de5d711d97
xkuubix <buler.jakub@gmail.com>
2022-07-23 00:20:38 +0200

    added early stopping, image showing in test phase

--- net_utils.py
+++ net_utils.py
@@ -6,6 +6,7 @@ import numpy as np
 # from deactivate_batchnorm import deactivate_batchnorm
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import classification_report
+from my_drawings import my_show_image
 
 
 def train_net(net, dataloaders,
@@ -22,13 +23,14 @@ def train_net(net, dataloaders,
     """
     since = time.time()
     best_net_wts = copy.deepcopy(net.state_dict())
-    best_acc = 0.0
+    best_loss = None
+    early_stopping_counter = 0
+    patience = 10
     accuracy_stats = {"train": [], "val": []}
     loss_stats = {"train": [], "val": []}
 
     for epoch in range(num_epochs):
-        print(f'Epoch {epoch+1}/{num_epochs}')
-        print('-' * 10)
+        print(f'Epoch {epoch+1 :3}/{num_epochs}', end=' ')
 
         for phase in ["train", "val"]:
             if phase == "train":
@@ -61,8 +63,8 @@ def train_net(net, dataloaders,
                     # backward pass + opt
                     if phase == 'train':
                         loss.backward()
+                        # scheduler.step()  # ----------------------------
                         optimizer.step()
-                        scheduler.step()  # ----------------------------
 
                 # statistics
                 phase_loss += loss.item() * images.size(0)
@@ -73,72 +75,79 @@ def train_net(net, dataloaders,
                 if str(criterion) == 'CrossEntropyLoss()':
                     phase_corrects += torch.sum(preds == labels)
 
-            # if phase == 'train':
-            #     scheduler.step()
+            if phase == 'train':
+                LR_val = scheduler.get_last_lr()[0]
+                scheduler.step()  # ----------------------------
 
             epoch_loss = phase_loss / dataloaders_size[phase]
             epoch_acc = phase_corrects.double() / dataloaders_size[phase]
             loss_stats[phase].append(epoch_loss)
             accuracy_stats[phase].append(epoch_acc)
 
-            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+            time_e = time.time() - since
+            print(f'| {phase} loss: {epoch_loss:.4f} - ' +
+                  f'acc: {epoch_acc:.4f}', end=' ')
+            if phase == 'val':
+                print(f'| LR: {LR_val:.6f} | '
+                      + f'time: {time_e // 60:3.0f}m {time_e % 60:2.0f}s')
 
             # deep copy the net params
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
+            if phase == 'val' and epoch_loss < best_loss or best_loss is None:
+                best_loss = epoch_loss
                 best_net_wts = copy.deepcopy(net.state_dict())
-        print()
+                early_stopping_counter = 0
+            elif phase == 'val' and epoch_loss > best_loss:
+                early_stopping_counter += 1
+        # early stopping
+        if early_stopping_counter >= patience:
+            print('INFO: Early stopping!')
+            break
 
     time_e = time.time() - since
     print(f'Training completed in {time_e // 60:.0f}m {time_e % 60:.0f}s')
-    print(f'Best val Acc: {best_acc:4f}')
+    print(f'Best val Loss: {best_loss:4f}')
 
     # load best net weights
     net.load_state_dict(best_net_wts)
     return net, loss_stats, accuracy_stats
 
 
-def test_net(net, data_loaders: dict, class_names: list, device):
+def test_net(net, data_loaders: dict, class_names: list, device,
+             drawing_num=0):
 
     net.eval()
     preds = np.array([])
     true = np.array([])
-    l1 = list()
-    l2 = list()
-    l3 = list()
-    l4 = list()
-
+    drawings = 0
     for batches in data_loaders["test"]:
 
         images = batches[0].to(device)
-        # images = images.repeat(1, 3, 1, 1)
         targets = batches[1]['labels'].to(device)
 
         with torch.no_grad():
             outputs = net(images)
             if outputs.size(dim=1) == 1:
-                preds = np.append(preds, torch.sigmoid(
-                    outputs).reshape(-1).detach().cpu().numpy().round())
+                pred = torch.sigmoid(
+                    outputs).reshape(-1).detach().cpu().numpy().round()
+                preds = np.append(preds, pred)
             elif outputs.size(dim=1) == 4:
-                preds = np.append(preds, outputs.softmax(1).argmax(1).cpu())
+                pred = outputs.softmax(1).argmax(1).cpu()
+                preds = np.append(preds, pred)
             true = np.append(true, targets.cpu())
 
-        for img in range(len(targets)):
-            p = torch.sigmoid(
-                    outputs).reshape(-1).detach().cpu().numpy().round()
-            if batches[1]["class"][img] == 'Normal':
-                l1.append(p[img])
-            if batches[1]["class"][img] == 'Benign':
-                l2.append(p[img])
-            if batches[1]["class"][img] == 'Malignant':
-                l3.append(p[img])
-            if batches[1]["class"][img] == 'Lymph_nodes':
-                l4.append(p[img])
+            #  plot images and predictions
+            for i in range(len(images)):
+                if drawings == drawing_num:
+                    break
+                else:
+                    dcm = [batches[0][i], {'view': batches[1]['view'][i],
+                                           'class': batches[1]['class'][i]}]
+                    prediction = int(preds[drawings])
+                    my_show_image(dcm, with_marks=True,
+                                  prediction=prediction,
+                                  class_names=class_names)
+                    drawings += 1
 
-    print(f'l1:{l1}\nSum:{sum(i for i in l1)}\n'
-          + f'l2:{l2}\nSum:{sum(i for i in l2)}\n'
-          + f'l3:{l3}\nSum:{sum(i for i in l3)}\n'
-          + f'l4:{l4}\nSum:{sum(i for i in l4)}')
     cm = confusion_matrix(true, preds)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
     disp.plot()
