64b44ece70983d02688a1b3fd93ed026197ca9cd
xkuubix <buler.jakub@gmail.com>
2022-08-08 00:46:17 +0200

    new

---
+++ batch_idx_sel.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def batched_index_select(input, dim, index):
+    for ii in range(1, len(input.shape)):
+        if ii != dim:
+            index = index.unsqueeze(ii)
+    expanse = list(input.shape)
+    expanse[0] = -1
+    expanse[dim] = -1
+    index = index.expand(expanse)
+    return torch.gather(input, dim, index)
