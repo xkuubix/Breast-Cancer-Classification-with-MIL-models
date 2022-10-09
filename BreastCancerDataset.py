ebcc5f42582648d31b586a64c8f61677b5ae9838
xkuubix <buler.jakub@gmail.com>
2022-10-10 00:08:23 +0200

    added multiscale handling (+max changed to 4095)

--- BreastCancerDataset.py
+++ BreastCancerDataset.py
@@ -1,7 +1,7 @@
 from pydicom import dcmread
 import torch
 import os
-# from torchvision import transforms as T
+from torchvision import transforms as T
 import torchvision.transforms.functional as TF
 from skimage import img_as_float32
 from tile_maker import convert_img_to_bag
@@ -33,10 +33,10 @@ class BreastCancerDataset(torch.utils.data.Dataset):
 
         img = dcm.pixel_array
         height, width = img.shape
-
+        img = img/4095
         img = img_as_float32(img)
         img = torch.from_numpy(img).unsqueeze(0).repeat(3, 1, 1)
-        img = img/torch.max(img)
+        # img = img/torch.max(img)
 
         target = {}
         # CCE long 0 1 2 3, BCE float 0. 1.
@@ -65,19 +65,45 @@ class BreastCancerDataset(torch.utils.data.Dataset):
         #     t = T.RandomHorizontalFlip(p=1.0)
         #     img = t(img)
 
-        if self.transforms is not None:
-            img = self.transforms(img)
-
         if self.convert_to_bag:
             target['full_image'] = img
-            img, t_id = convert_img_to_bag(img, self.tiles, self.bag_size)
-            target['tiles_indices'] = t_id
-            if self.transforms is not None:
-                angle = random.choice([-90, 0, 90, 180])
-                img = TF.rotate(img, angle)
+            # Multi scale bag instances
+            if len(self.tiles) == 2:
+                if target["laterality"] == 'R':
+                    t = T.RandomHorizontalFlip(p=1.0)
+                    img = t(img)
 
-            # t = T.Normalize(mean=[0.2347, 0.2347, 0.2347],
-            # std=[0.1602, 0.1602, 0.1602])
+                instances_scale_1, t_id_scale_1 = convert_img_to_bag(
+                    img, self.tiles[0], self.bag_size)
+                instances_scale_2, t_id_scale_2 = convert_img_to_bag(
+                    img, self.tiles[1], self.bag_size)
+                t = T.Resize(224)
+                instances_scale_2 = t(instances_scale_2)
+                img = torch.cat((instances_scale_1, instances_scale_2),
+                                dim=0)
+                if self.transforms is not None:
+                    for i, image in enumerate(img):
+                        angle = random.choice([-90, 0, 90, 180])
+                        img[i] = TF.rotate(img[i], angle)
+                        # img[i] = self.transforms(img[i])
+                    img = self.transforms(img)
+                target['tiles_indices'] = [t_id_scale_1, t_id_scale_2]
+            # Single scale bag instances
+            else:
+                img, t_id = convert_img_to_bag(img, self.tiles, self.bag_size)
+                target['tiles_indices'] = t_id
+                if self.transforms is not None:
+                    for i, image in enumerate(img):
+                        angle = random.choice([-90, 0, 90, 180])
+                        img[i] = TF.rotate(img[i], angle)
+                        # img[i] = self.transforms(img[i])
+                    img = self.transforms(img)
+
+            # mean = [0.2347, 0.2347, 0.2347]
+            # std = [0.1602, 0.1602, 0.1602]
+            # mean = [0.485, 0.456, 0.406]
+            # std = [0.229, 0.224, 0.225]
+            # t = T.Normalize(mean, std)
             # img = t(img)
 
         return img, target
@@ -86,7 +112,7 @@ class BreastCancerDataset(torch.utils.data.Dataset):
         return len(self.dicoms)
 
     def __select_view(self):
-        '''Select only one view and return list of filenames
+        '''Select only given view(s) and return list of filenames
            and class names(folder names)
         '''
         # 0x0018, 0x5101 - View Position
@@ -98,10 +124,17 @@ class BreastCancerDataset(torch.utils.data.Dataset):
             for item in range(len(patient['class'])):
                 for v in self.view:
                     if patient['view'][item].__contains__(v):
-                        # if patient['class'][item].find('Lymph_nodes') is -1:
                         class_names_list.append(patient['class'][item])
                         filenames_list.append(patient['filename'][item])
                         view_list.append(patient['view'][item])
+        # if patient['class'][item].find('Malignant') is not -1:
+        #     class_names_list.append(patient['class'][item])
+        #     filenames_list.append(patient['filename'][item])
+        #     view_list.append(patient['view'][item])
+        # if patient['class'][item].find('Lymph_nodes') is not -1:
+        #     class_names_list.append(patient['class'][item])
+        #     filenames_list.append(patient['filename'][item])
+        #     view_list.append(patient['view'][item])
 
         return view_list, filenames_list, class_names_list
 
