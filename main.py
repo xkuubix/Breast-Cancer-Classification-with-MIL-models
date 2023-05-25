8996835b96e815807829ba438f74cdda09e64cc0
xkuubix <buler.jakub@gmail.com>
2023-05-25 14:41:10 +0200

    Changes: New machine  path, new args from config

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # %%
+import pandas as pd
 import torch
 from torchvision import transforms as T
 from my_drawings import my_show_image, my_show_training_results
@@ -10,6 +11,8 @@ from random_split_df import random_split_df
 from LRFinder import LRFinder, plot_lr_finder
 from get_points_to_crop import get_points
 from tile_maker import get_tiles
+import os
+import numpy as np
 
 import matplotlib.pyplot as plt  # temp
 # from tile import convert_img_to_bag, get_tiles  # temp
@@ -19,11 +22,12 @@ import yaml
 import neptune.new as neptune
 import uuid
 import copy
-
-
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning)
+# %%
 # MAKE PARSER AND LOAD PARAMS FROM CONFIG FILE--------------------------------
 def get_args_parser():
-    default = '/home/student2/Projekt_MMG/config.yml'
+    default = '/home/jr_buler/Projekt_MMG/config.yml'
     help = '''path to .yml config file
     specyfying datasets/training params'''
 
@@ -63,9 +67,14 @@ val_test_frac = config['data_sets']['split_fraction_val_test']
 bag_size_train = config['data_sets']['bag_size_train']
 bag_size_val_test = config['data_sets']['bag_size_val_test']
 
+image_size = config['image']['size']
+image_multimodality = config['image']['multimodal']
+
 patch_size = config['data_sets']['patch_size']
 overlap_train = config['data_sets']['overlap_train_val']
 overlap_val_test = config['data_sets']['overlap_val_test']
+
+# %%
 
 # TRANSFORMS------------------------------------------------------------------
 cj_prob = 0.5
@@ -81,11 +90,12 @@ min_scale = 0.8
 color_jitter = T.ColorJitter(cj_bright, cj_contrast, cj_sat, cj_hue)
 gaussian_blur = T.GaussianBlur(kernel_size=23, sigma=(0.1, 2.0))
 
-transform = T.Compose([T.RandomAffine(degrees=(0), translate=(0, 0.1)),
+transform = T.Compose([  # T.RandomAffine(degrees=(0), translate=(0, 0.1)),
                        T.RandomHorizontalFlip(),
                        T.RandomVerticalFlip(),
                        T.RandomResizedCrop(size=input_size,
-                                           scale=(min_scale, 1.0)),
+                                           scale=(min_scale, 1.0),
+                                           antialias=True),
                        # T.RandomApply([color_jitter], p=cj_prob),
                        # T.RandomApply([gaussian_blur], p=gaussian_blur_prob)
                        ])
@@ -94,10 +104,10 @@ transforms_val_test = None
 tfs = [transform, transforms_val_test, transforms_val_test]
 
 # GET TILES (PATCHES) COORDS (size/overlap)-----------------------------------
-tiles_train = get_tiles(3518, 2800//2,
+tiles_train = get_tiles(image_size[0], image_size[1],
                         patch_size, patch_size,
                         overlap_train)
-tiles_test_val = get_tiles(3518, 2800//2,
+tiles_test_val = get_tiles(image_size[0], image_size[1],
                            patch_size, patch_size,
                            overlap_val_test)
 tiles = [tiles_train, tiles_test_val]
@@ -116,11 +126,27 @@ df = make_df(root,
              from_file=True,
              save_to_file=False,
              file_dir=file_dir)
+
+
+# ***************
+# os.chdir('/media/dysk/student2/CMMD/TheChineseMammographyDatabase/')
+# df = pd.read_csv('CMMD_clinicaldata_revision_CSV.csv', sep=';')
+# df = df.sort_values(
+#     by=['LeftRight']).groupby('ID1').agg({"Age": np.mean,
+#                                           "LeftRight": list,
+#                                           "classification": list,
+#                                           }).reset_index()
+# root = '/media/dysk/student2/CMMD/TheChineseMammographyDatabase/CMMD/'
+# ***************
+
+# %%
+
 if create_new_dl:
     train_set, val_set, test_set = random_split_df(df,
                                                    train_rest_frac,
                                                    val_test_frac,
                                                    seed=seed)
+
     data_loaders, data_loaders_sizes, ds = gen_data_loader(
         root=root,
         train_df=train_set,
@@ -133,7 +159,9 @@ if create_new_dl:
         conv_to_bag=True,
         bag_size_train=bag_size_train,
         bag_size_val_test=bag_size_val_test,
-        tiles=tiles
+        tiles=tiles,
+        img_size=image_size,
+        is_multimodal=image_multimodality
         )
     torch.save(data_loaders, dataloader_path)
     torch.save(data_loaders_sizes, dataloader_sizes_path)
@@ -160,7 +188,6 @@ if 0:
             my_show_image(ds['test'][i], with_marks=True, format_type='svg')
 # %%
 if 0:
-    import os
     from pydicom import dcmread
     dir = root
     os.chdir(dir)
@@ -203,7 +230,10 @@ if 0:
 EPOCHS = config['training_plan']['parameters']['epochs'][0]
 lr = config['training_plan']['parameters']['lr'][0]
 wd = config['training_plan']['parameters']['wd'][0]
+grad_accu = config['training_plan']['parameters']['grad_accu']['is_on']
+grad_accu_steps = config['training_plan']['parameters']['grad_accu']['steps']
 net_ar = config['training_plan']['architectures']['names']
+net_ar_dropout = config['training_plan']['architectures']['dropout']
 criterion_type = config['training_plan']['criterion']
 optimizer_type = config['training_plan']['optim_name']
 scheduler_type = config['training_plan']['scheduler']
@@ -214,6 +244,7 @@ if scheduler_type == 'one-cycle':
 
 net, criterion, optimizer, scheduler = choose_NCOS(
     net_ar=net_ar,
+    dropout=net_ar_dropout,
     device=device,
     pretrained=True,
     criterion_type=criterion_type,
@@ -233,39 +264,26 @@ if 1:
             net.running_var = None
             # net.momentum = 0.01
 
+    # def change_bn_to_gn(net):
+    #     for name, module in net.named_modules():
+    #         if isinstance(module, nn.BatchNorm2d):
+    #             # Get current bn layer
+    #             bn = getattr(net, name)
+    #             # Create new gn layer
+    #             gn = nn.GroupNorm(1, bn.num_features)
+    #             # Assign gn
+    #             print('Swapping {} with {}'.format(bn, gn))
+    #             setattr(net, name, gn)
+
     net.apply(deactivate_batchnorm)
 if 0:
-    # fn = 'clr_lr-1loss2.578792095184326'
-    fn = 'clr_lr-0.001loss2.6703104972839355'
-    model_load_path = ('/media/dysk/student2/mammografia/Zapisy/'
-                       + 'clr_saved_models/' + fn)
-    sd = torch.load(model_load_path, map_location=device)
+    fn = '966ccf26-e250-4ea8-a5f5-9fa7fc9287d8'
+    model_load_path = ('/media/dysk_a/jr_buler/mammografia/Zapisy/'
+                       + 'neptune_saved_models/' + fn)
+    net_sd = torch.load(model_load_path, map_location=device)
     net_sd = copy.deepcopy(net.state_dict())
-    # fe = 'feature_extractor'
-    fe = 'backbone'
 
-    net_fe_layers = list()
-    for name, param in net_sd.items():
-        if 'feature_extractor' in name:
-            net_fe_layers.append(name)
-
-    for i, (name, param) in enumerate(sd.items()):
-        if fe in name:
-            param = param.data
-            net_sd[net_fe_layers[i]].copy_(param)
     net.load_state_dict(net_sd)
-
-    # ct = 0
-    # gct = 0
-    # for child in net.children():
-    #     ct += 1
-    #     if ct == 1:
-    #         for grandchild in child.children():
-    #             gct += 1
-    #             if gct > 7:
-    #                 for param in grandchild.parameters():
-    #                     param.requires_grad = False
-
     net.to(device)
 # %%
 # TRAIN NETWORK---------------------------------------------------------------
@@ -281,7 +299,10 @@ state_dict_BL, state_dict_BACC, loss_stats, accuracy_stats = train_net(
     device, criterion,
     optimizer, scheduler,
     num_epochs=EPOCHS,
-    neptune_run=run)
+    neptune_run=run,
+    grad_acc_mode=grad_accu,
+    accum_steps=grad_accu_steps
+    )
 
 if 0:
     my_show_training_results(accuracy_stats, loss_stats)
@@ -335,79 +356,6 @@ if run is not None:
     run['test/auc_roc'] = roc_auc
     run.stop()
 # %%
-if 0:
-    std = torch.load('/media/dysk/student2/mammografia/Zapisy/'
-                     + 'neptune_saved_models/'
-                     + 'd855fb28-53c8-416f-9fa6-099f3b5a6b2b',
-                     map_location=device)
-    net.load_state_dict(std)
-# %%
-if 0:
-    with torch.no_grad():
-        for i in range(1):
-            empty_img = torch.zeros(3, 3518, 2800)
-            to_show = torch.zeros(3, 3518, 2800)
-
-            d_s = next(iter(data_loaders['test']))
-            im = d_s[1]['full_image'].squeeze(dim=0)
-            id = d_s[1]['tiles_indices'].squeeze(dim=0)
-
-            net.eval()
-            input = d_s[0].to(device)
-            output = net(input, d_s[1]['tile_cords'])
-            pred = torch.sigmoid(output[0]).detach().cpu().numpy().round()
-            # _, pred = torch.max(output[0].reshape(-1, 4), 1)
-            # if pred != d_s[1]['labels'] or d_s[1]['labels'] == 0:
-            #     continue
-            weights = output[1]
-
-            # weights = torch.sum(output[2], dim=1)
-            plt.matshow(output[2].cpu().numpy())
-            weights = weights.squeeze()
-
-            for item in range(len(id)):
-                h_min, w_min, dh, dw, _, _ = tiles[1][id][item]
-                to_show[:, h_min:h_min+dh, w_min:w_min+dw] = 1
-
-                # if ind[item] == 0:
-                #     empty_img[:, h_min:h_min+dh, w_min:w_min+dw] +=\
-                #         weights[item][pred].detach().cpu()
-
-                empty_img[:, h_min:h_min+dh, w_min:w_min+dw] +=\
-                    weights[item].detach().cpu()
-
-                # empty_img[:, h_min:h_min+dh, w_min:w_min+dw] =\
-                #     torch.max(empty_img[:, h_min:h_min+dh, w_min:w_min+dw],
-                #               weights[item].detach().cpu())
-            empty_img -= torch.min(weights.reshape(-1).cpu())
-            empty_img[empty_img < 0] = 0
-            empty_img /= torch.max(empty_img.reshape(-1))
-            # empty_img[empty_img < 0.9] = 0
-            # empty_img /= torch.max(empty_img.reshape(-1))
-            # empty_img *= 100
-
-            fig, ax = plt.subplots(1, 3, figsize=(35//2, 28//2))
-            ax[0].imshow(im.permute(1, 2, 0), cmap='gray')
-            ax[1].imshow(to_show.permute(1, 2, 0), cmap='gray')
-            ax[2].imshow(empty_img.permute(1, 2, 0), cmap='gray')
-
-            if d_s[1]['class'][0] == 'Normal':
-                s_title = ' ' + 'Ground Truth: Normal'
-            elif d_s[1]['class'][0] == 'Benign':
-                s_title = ' ' + 'Ground Truth: Benign'
-            elif d_s[1]['class'][0] == 'Malignant':
-                s_title = ' ' + 'Ground Truth: Malignant'
-            elif d_s[1]['class'][0] == 'Lymph_nodes':
-                s_title = ' ' + 'Ground Truth: Lymph_nodes'
-            ax[0].set_title(s_title)
-            ax[1].set_title("Selected tiles")
-            # predictions = ['No cancer', ' Cancer']
-            predictions = class_names
-            # bce / ce
-            # pred = torch.sigmoid(output[0]).detach().cpu().numpy().round()
-            s_title = "Predicted: " + predictions[int(pred[0])]
-            ax[2].set_title(s_title)
-# %%
 
 # psum  = torch.tensor([0.0, 0.0, 0.0])
 # psum_sq = torch.tensor([0.0, 0.0, 0.0])
@@ -416,7 +364,6 @@ if 0:
 # # dataloader: B N c h w
 # # bag:          N c h w
 # # tile:           c h w
-
 
 # for batch in data_loaders['test']:
 #     for bag in batch[0]:
@@ -436,4 +383,99 @@ if 0:
 # # output
 # print('mean: '  + str(total_mean))
 # print('std:  '  + str(total_std))
+# %%
+# Rysowanie hist dla setow val i test
+# confidence dla kazdego pacjenta w zbiorze
+
+# 34a2036b-0f9c-4dc5-b583-86ec05c932e3  best rocacc
+# eaedf20a-8cac-479f-a5f3-07727bf3b726  best roclss
+if 0:
+
+    std = torch.load('/media/dysk/student2/mammografia/Zapisy/'
+                     + 'neptune_saved_models/'
+                     + '34a2036b-0f9c-4dc5-b583-86ec05c932e3',
+                     map_location=device)
+    net.load_state_dict(std)
+    phases = ['val', 'test']
+    net.to(device)
+    net.eval()
+
+    ids_list = list()
+    pred_list = list()
+    class_list = list()
+    for phase in phases:
+        for images, targets in data_loaders[phase]:
+            images = images.to(device)
+            labels = targets["labels"].to(device)
+            with torch.no_grad():
+                outputs = net(images)
+                preds = torch.sigmoid(
+                    outputs[0]).reshape(-1).detach().cpu()
+
+                if preds.numpy().round() == labels.item():
+                    ids_list.append(targets['patient_id'])
+                    pred_list.append(preds.item())
+                    class_list.append(targets['class'])
+    pred_df = pd.DataFrame()
+    pred_df['scores'] = pred_list
+    pred_df['class'] = class_list
+    pred_df['id'] = ids_list
+    pred_df['scores'].plot.hist(grid=True, bins=20, rwidth=0.9,
+                                color='#607c8e')
+    plt.title('Correct preds')
+    plt.xlabel('Scores')
+    plt.ylabel('Counts')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+
+    ids_list = list()
+    pred_list = list()
+    class_list = list()
+    laterality_list = list()
+    for phase in phases:
+        for images, targets in data_loaders[phase]:
+            images = images.to(device)
+            labels = targets["labels"].to(device)
+            with torch.no_grad():
+                outputs = net(images)
+                preds = torch.sigmoid(
+                    outputs[0]).reshape(-1).detach().cpu()
+
+                if preds.numpy().round() != labels.item():
+                    ids_list.append(targets['patient_id'])
+                    pred_list.append(preds.item())
+                    class_list.append(targets['class'])
+                    laterality_list.append(targets['laterality'])
+    pred_df = pd.DataFrame()
+    pred_df['scores'] = pred_list
+    pred_df['class'] = class_list
+    pred_df['id'] = ids_list
+    pred_df['scores'].plot.hist(grid=True, bins=20, rwidth=0.9,
+                                color='#607c8e')
+    print("False negatives")
+    print(pred_df[pred_df['scores'] < 0.2])
+    print("False positives")
+    print(pred_df[pred_df['scores'] > 0.8])
+    plt.title('Incorrect preds')
+    plt.xlabel('Scores')
+    plt.ylabel('Counts')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+# %%
+if 0:
+
+    phases = ['train', 'val', 'test']
+    num_instances = list()
+    for phase in phases:
+        for images, targets in data_loaders[phase]:
+            num_instances.append(len(images[0]))
+        df = pd.DataFrame()
+        df['num_instances'] = num_instances
+        df['num_instances'].plot.hist(grid=True, bins=20, rwidth=0.9,
+                                      color='#607c8e')
+        plt.title('Bag sizes' + ' [' + phase + ']')
+        plt.xlabel('Number of instances')
+        plt.ylabel('Bags')
+        plt.grid(axis='y', alpha=0.75)
+        plt.show()
 # %%
