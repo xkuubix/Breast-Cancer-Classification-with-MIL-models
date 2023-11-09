95c078d704afc4cf6f9bc3d2c0e3b6510b86740f
xkuubix <buler.jakub@gmail.com>
2023-11-09 12:24:09 +0100

    best metric selecting changed

--- draw_cv_results.py
+++ draw_cv_results.py
@@ -2,28 +2,48 @@
 import neptune.new as neptune
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 import re
 sns.set()
-
+# %%
 data = neptune.init_project(name='ProjektMMG/5-fold-CV-Cancers')
 
-# Get dashboard with runs contributed by "jackie" and tagged "cycleLR"
 markers = ['r.-', 'go-', 'bv-', 'kx-', 'y*-', 'cD-', 'mH-']
 marker = 0
 tags = [
         # 'nr 12',
-        'nr 1',
-        # 'nr 16',
-        'nr 7',
+        # 'nr 2',
+        'nr 19',
+        'nr 20',
+        # 'a1',
         # 'nr 21',
-        'nr 8',
+        # 'nr 8',
         # 'nr 18', 'nr 9', 'nr 11', 'nr 17', 'nr 14',
         # 'nr 2', 'nr 3',
-        'nr 5', 
+        # 'nr 5',
         # 'nr 4', 'nr 13',
         # 'nr 5', 'nr 15', 'nr 10', 'nr 6',
         # 'nr 2', 'nr 5', 'nr 19', 'nr 20', 'nr 22'
         # 'nr 19', 'nr 20'
+        # 'n1',
+        # 'n2',
+        # 'n4'
+        # '1n1',
+        # '2n2',
+        # '3n3',
+        # '4n4'
+        # 'a6',
+        # 'a5',
+        # 'a2',
+        # 'a3',
+        'a4',
+        'a10',
+        # 'nr 5'
+        # 'a7',
+        # 'a8',
+        # 'a9',
+        # 'a10'
+        # 'nr 5'
         ]
 
 f1s = []
@@ -36,6 +56,12 @@ for tag in tags:
     marker += 1
     runs_table_df = data.fetch_runs_table(owner=["rbuler", 'jakub-buler'],
                                           tag=tag).to_pandas()
+    # print(runs_table_df['sys/monitoring_time'])
+    mean_time = runs_table_df['sys/monitoring_time'].mean()
+    hours = mean_time // 3600
+    minutes = (mean_time % 3600) // 60
+    print(int(hours), ':', int(minutes))
+    # %%
 
     k = max(runs_table_df['fold-k']) + 1
     metrics = {'f1': [], 'acc': [], 'auc': [],
@@ -50,11 +76,11 @@ for tag in tags:
         bloss = runs_table_df['test/auc_roc'][
             runs_table_df['fold-k'] == i].values[0]
         if bloss >= bacc:
-            s = str(runs_table_df[['test/BL/metrics_th_best']][
+            s = str(runs_table_df[['test/BL/metrics']][
                 runs_table_df['fold-k'] == i].values)
             auc = bloss
         else:
-            s = str(runs_table_df[['test/BACC/metrics_th_best']][
+            s = str(runs_table_df[['test/BACC/metrics']][
                 runs_table_df['fold-k'] == i].values)
             auc = bacc
         values = re.findall(
@@ -96,41 +122,43 @@ for tag in tags:
     # print(metrics)
     # Creating axes instance
 
-
+# %%
 def bp(data, labels_x, label_y, fig, sp):
     # fig = plt.figure(figsize=(8, 4))
     ax = fig.add_subplot(sp)
     bplot = ax.boxplot(data, patch_artist=True,
                        notch=None, vert=1)
 
-    colors = [ 'red', 'lightgreen',
-            #   'red', 'red', 'red', 'red', 'red',
-              'lightgreen', 'lightgreen', 'lightgreen',
-              'lightgreen', 'lightgreen',
-            ]
+    colors = [
+        '#212121', '#424242', '#757575', '#455A64', '#546E7A', '#78909C',
+        '#212121', '#424242', '#757575', '#455A64', '#546E7A', '#78909C']
     for patch, color in zip(bplot['boxes'], colors):
         patch.set_facecolor(color)
+    for median in bplot['medians']:
+        median.set(color='red', linewidth=3)
 
-    ax.set_xticklabels(labels_x)
+    # ax.set_xticklabels(labels_x)
     title = '5-fold CV'
     if sp == 232:
         plt.title(title)
     plt.ylabel(label_y)
     # ax.get_xaxis().tick_bottom()
     if sp == 235:
-        import matplotlib.patches as mpatches
+        # import matplotlib.patches as mpatches
         ax.get_yaxis().tick_left()
         plt.xticks(rotation=0)
-        # p1 = mpatches.Patch(color='red', label='Without edge artifacts')
-        # p2 = mpatches.Patch(color='lightgreen', label='With edge artifacts')
-        # p3 = mpatches.Patch(color='#eaeaf2',  label='Signle View CC or MLO')
-        # p4 = mpatches.Patch(color='#eaeaf200',  label='1) overlap 0.      patch size 224')
-        # p5 = mpatches.Patch(color='#eaeaf200',  label='2) overlap 0.25  patch size 224')
-        # p6 = mpatches.Patch(color='#eaeaf200',  label='3) overlap 0.5    patch size 224')
-        # p7 = mpatches.Patch(color='#eaeaf200',  label='4) overlap 0.5    patch size 128')
-        # p8 = mpatches.Patch(color='#eaeaf200',  label='5) overlap 0.5    patch size 448')
+        # p1 = mpatches.Patch(color=colors[0], label='')
+        # p2 = mpatches.Patch(color=colors[1], label='')
+        # p3 = mpatches.Patch(color=colors[2],  label='')
+        # p4 = mpatches.Patch(color=colors[3],  label='')
+        # p5 = mpatches.Patch(color=colors[4],  label='')
+        # p6 = mpatches.Patch(color=colors[5],  label='')
+        # p6 = mpatches.Patch(color=colors[0],  label='')
+        # p7 = mpatches.Patch(color='#eaeaf200',  label='')
+        # p8 = mpatches.Patch(color='#eaeaf200',  label='')
         # plt.legend(handles=[p1, p2, p3])
-        # plt.legend(handles=[p1, p2], loc='center left',
+        # plt.legend(handles=[p1, p2, p3, p4,
+        #                     p5, p6], loc='center left',
         #            bbox_to_anchor=(1.25, 0.5),
         #            fontsize='large')
         plt.show()
@@ -157,11 +185,12 @@ tags = [
         '2',
         '3',
         '4',
-        # '5',
-        # '1',
-        # '2',
-        # '3',
-        # '4',
+        '5',
+        '6'
+        # '7',
+        # '8',
+        # '9',
+        # '10',
         # '5'
         # 'ResNet\n18',
         # 'ResNet\n34',
@@ -180,8 +209,6 @@ bp(recs, tags, 'Recall', fig=fig, sp=235)
 
 data.stop()
 # %%
-import numpy as np
-
 np_aucs = np.array(aucs)
 np_accs = np.array(accs)
 np_f1s = np.array(f1s)
@@ -204,5 +231,5 @@ for i in range(len(tags)):
 
     print("{0:.3f}".format(np_recs.mean(axis=1)[i]), end=' ')
     print("{0:.3f}".format(np_recs.std(axis=1)[i]), end=' ')
-    
+
 # %%
